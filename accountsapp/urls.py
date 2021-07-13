c88403f3e2e70be66e8ba52297346094aaeb5edf
yangsunkang <dayoorang@gmail.com>
2021-07-13 12:39:15 +0900

    07/13 project_2 practice 2

--- accountsapp/urls.py
+++ accountsapp/urls.py
@@ -4,4 +4,6 @@ from .views import hello_world
 app_name = 'first_app'
 urlpatterns = [
     path('hello_world/', hello_world, name = 'hello_world')
+    path('create/', AccountCreateView.as_view(), name='create')
+
 ]
--- accountsapp/views.py
+++ accountsapp/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
 from accountsapp.models import NewModel
 
 
@@ -14,10 +19,16 @@ def hello_world(request):
 
         data_list = NewModel.objects.all()
 
-        return render(request, 'accountsapp/hello_world.html',
-                      context={'data_list': data_list})
+        return HttpResponseRedirect(reverse('first_app:hello_world'))
     else:
         data_list = NewModel.objects.all()
 
         return render(request, 'accountsapp/hello_world.html',
                       context={'data_list': data_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('first_app:hello_world')
+    template_name = 'accountsapp/create.html'
