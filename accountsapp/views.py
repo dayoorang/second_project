dc359c10b62595021d4820c993850b3273b3f69f
yangsunkang <dayoorang@gmail.com>
2021-07-13 11:18:09 +0900

    07/13 project_2 practice 1

--- accountsapp/views.py
+++ accountsapp/views.py
@@ -2,10 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountsapp.models import NewModel
+
+
 def hello_world(request):
     if request.method == 'POST':
+        temp = request.POST.get('input_text')
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+
+        data_list = NewModel.objects.all()
+
         return render(request, 'accountsapp/hello_world.html',
-                      context={'text': 'POST METHOD'})
+                      context={'data_list': data_list})
     else:
+        data_list = NewModel.objects.all()
+
         return render(request, 'accountsapp/hello_world.html',
-                      context={'text': 'GET METHOD'})
+                      context={'data_list': data_list})
