580822b5df1d893a037300f99b738a87fc0a65ff
yangsunkang <dayoorang@gmail.com>
2021-07-23 12:50:42 +0900

    07/23 project_2 practice 3

--- second_project/urls.py
+++ second_project/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountsapp.urls'))
-    path('profiles/', include('profileapp.urls'))
+    # path('profiles/', include('profileapp.urls'))
 
 ]
