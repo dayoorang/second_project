014d63c7788cb040712c8b51321383ece3167b0a
yangsunkang <dayoorang@gmail.com>
2021-07-27 11:55:07 +0900

    07/27 project_2 practice 2

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreationView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    template_name = 'profileapp/create.html'
+    success_url = reverse_lazy('first_app:hello_world')
--- second_project/urls.py
+++ second_project/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accountsapp.urls'))
-    # path('profiles/', include('profileapp.urls'))
+    path('accounts/', include('accountsapp.urls')),
+    path('profiles/', include('profileapp.urls'))
 
 ]
