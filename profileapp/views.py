d1cff3f6697baee8e5d3f3693ea64ea75e09ecfb
yangsunkang <dayoorang@gmail.com>
2021-07-27 12:26:13 +0900

    07/27 project_2 practice 3

--- profileapp/views.py
+++ profileapp/views.py
@@ -13,3 +13,8 @@ class ProfileCreationView(CreateView):
     form_class = ProfileCreationForm
     template_name = 'profileapp/create.html'
     success_url = reverse_lazy('first_app:hello_world')
+
+
+    def form_valid(self, form): # 다 검증이 완료된후 실행되는 함수.. FBV 에서는 form.is_valid() 인듯..
+        form.instance.user = self.request.user # form 안에 것들을 저장할 그때당시의 user / form.user X
+        return super().form_valid(form)
