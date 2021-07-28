2551ec3607164682f0718469847128cc7bcaf8aa
yangsunkang <dayoorang@gmail.com>
2021-07-28 17:00:51 +0900

    07/28 project_2 practice 2

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from profileapp.views import ProfileCreationView
+from profileapp.views import ProfileCreationView, ProfileUpdateView
 
 app_name = 'profileapp'
 urlpatterns = [
-    path('create/', ProfileCreationView.as_view(), name = 'create' )
+    path('create/', ProfileCreationView.as_view(), name = 'create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update')
 
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
@@ -18,3 +18,10 @@ class ProfileCreationView(CreateView):
     def form_valid(self, form): # 다 검증이 완료된후 실행되는 함수.. FBV 에서는 form.is_valid() 인듯..
         form.instance.user = self.request.user # form 안에 것들을 저장할 그때당시의 user / form.user X
         return super().form_valid(form)
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_profile'
+    success_url = reverse_lazy('first_app:hello_world')
+    template_name = 'profileapp/update.html'
