a4d28cff1bcd6888b6592f5376637838b57d9f31
yangsunkang <dayoorang@gmail.com>
2021-07-28 17:40:11 +0900

    07/28 project_2 practice 4

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,13 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
+@method_decorator(login_required,'get')
+@method_decorator(login_required,'post')
 class ProfileCreationView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
@@ -19,6 +24,8 @@ class ProfileCreationView(CreateView):
         form.instance.user = self.request.user # form 안에 것들을 저장할 그때당시의 user / form.user X
         return super().form_valid(form)
 
+@method_decorator(profile_ownership_required,'get')
+@method_decorator(profile_ownership_required,'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
