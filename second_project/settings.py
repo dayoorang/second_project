6ebdab64fcdeeea221e8764ab7ee8e7bd84fe807
yangsunkang <dayoorang@gmail.com>
2021-07-30 12:09:39 +0900

    07/30 project_2 practice 3

--- second_project/settings.py
+++ second_project/settings.py
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accountsapp',
-    'profileapp'
+    'profileapp',
+    'articleapp'
 ]
 
 MIDDLEWARE = [
--- second_project/urls.py
+++ second_project/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountsapp.urls')),
-    path('profiles/', include('profileapp.urls'))
+    path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.urls'))
 
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
