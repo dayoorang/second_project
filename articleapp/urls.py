4045ba4f6e2e83834525151e7d5b155f64f28758
yangsunkang <dayoorang@gmail.com>
2021-08-04 17:47:20 +0900

    8/4 project_2 practice 3

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
 
 app_name = 'articleapp'
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
-    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update')
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete')
 
 ]
