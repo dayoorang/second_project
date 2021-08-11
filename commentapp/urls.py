5eced43598c8b4e5b4e6c166d8832402fd5557a6
yangsunkang <dayoorang@gmail.com>
2021-08-11 17:46:50 +0900

    8/11 project_2 practice 3

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -6,6 +6,6 @@ from commentapp.views import CommentCreationView, CommentDeleteView
 app_name = 'commentapp'
 urlpatterns = [
     path('create/', CommentCreationView.as_view(), name = 'create'),
-    path('delete/', CommentDeleteView.as_view(), name='delete'),
+    path('delete/<int:pk>', CommentDeleteView.as_view(), name='delete'),
 
 ]
