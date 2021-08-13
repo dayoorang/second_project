27609b008c34f61d972fd4780ffafcaa1675b998
yangsunkang <dayoorang@gmail.com>
2021-08-13 12:39:54 +0900

    8/13 project_2 practice 5

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,7 +1,12 @@
 
 from django.contrib import admin
+from django.urls import path
 
+from projectapp.views import ProjectCreateView
+
+
+app_name = 'projectapp'
 urlpatterns = [
 
-
+    path('create/', ProjectCreateView.as_view(), name='create')
 ]
