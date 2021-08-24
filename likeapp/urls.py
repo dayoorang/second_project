07c20ddfc3d12929e9b318215e3b454098bf84cc
yangsunkang <dayoorang@gmail.com>
2021-08-24 12:14:39 +0900

    8/24 project_1 practice 3

--- likeapp/urls.py
+++ likeapp/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path, include
 
+from likeapp.views import LikeArticleView
+
+app_name='likeapp'
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-
-
+    path('article/<int:article_pk>', LikeArticleView.as_view(), name='article_like'),
 ]
